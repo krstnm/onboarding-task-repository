2d3a1e31236c889a3cda2d7e043a0e0c8460bc0a
krstnm <75930370+krstnm@users.noreply.github.com>
2021-01-25 18:02:37 +1300

    Update Store.jsx

--- src/components/Shop/StoreFolder/Store.jsx
+++ src/components/Shop/StoreFolder/Store.jsx
@@ -1,10 +1,11 @@
 import React, {useEffect, useState} from "react";
-import Axios from "axios";
-import { Button, Table } from "semantic-ui-react";
+import { Button, Table, Select} from "semantic-ui-react";
 import "semantic-ui-css/semantic.min.css";
 import CreateStoreModal from "./CreateStoreModal";
 import EditStoreModal from "./EditStoreModal";
 import DeleteStoreModal from "./DeleteStoreModal";
+import $ from 'jquery';
+import ReactPaginate from 'react-paginate';
 
 const Store = (props) => {
   const [stores, setstores] = useState([]);
@@ -12,6 +13,13 @@ const Store = (props) => {
   const [openEdit, setopenEdit] = useState(false);
   const [opendelete, setopenDelete] = useState(false);
   const [store, setstore] = useState({});
+  const [order, setorder] = useState('ASC');
+  const [offset, setOffset] = useState(0);
+  const [data, setData] = useState([]);
+  const [perPage, setPerPage] = useState(5);
+  const [pageCount, setpageCount] = useState(0);
+  const [namedirection, setnamedirection] = useState();
+  const [addressdirection, setaddressdirection] = useState();
 
   useEffect(() => {
       if(!open){
@@ -30,17 +38,29 @@ const Store = (props) => {
       getStores();
     }
   }, [opendelete]);
-  
 
+  useEffect(() => {
+    getStores()
+  }, [offset, perPage])
 
   const getStores = () => {
-    Axios.get("/Stores/GetStore")
-        .then((res) => {
-            setstores(res.data)
-        })
-        .catch((err) => {
-            console.log(err)
-    });
+    $.ajax({
+        method: "GET",
+        url: "/Stores/GetStore",
+        dataType: 'json',
+        success: function(res){
+            setstores(res);
+            const slice = res.slice(offset, offset + perPage);
+            setData(slice);
+            setpageCount(Math.ceil(res.length / perPage));
+        }
+    })
+  }
+
+  const handlePaginationClick = (e) => {
+    const selectedPage = e.selected;
+    const offsets = selectedPage * perPage;
+    setOffset(offsets);
   }
 
   const handleEditStore = (store) => {
@@ -51,8 +71,55 @@ const Store = (props) => {
   const handleDeleteStore = (store) => {
     setstore(store);
     setopenDelete(true);
+  }
+
+  const onSort = (sortKey,order) => {
+    let sortStore = [...stores];
+    switch(sortKey){
+        case 'name':
+          if(order === 'DESC'){
+              sortStore.sort((a, b) => a[sortKey].localeCompare(b[sortKey]));
+              setnamedirection('descending');
+              setaddressdirection(null);
+              const slice = sortStore.slice(offset, offset + perPage);
+              setData(slice);
+              setorder('ASC');
+            }
+            if(order === 'ASC'){
+              sortStore.sort((a, b) => b[sortKey].localeCompare(a[sortKey]));
+              setnamedirection('ascending');
+              setaddressdirection(null);
+              const slice = sortStore.slice(offset, offset + perPage);
+              setData(slice);
+              setorder('DESC');
+            }
+          break;
+      case 'address':
+          if(order === 'DESC'){
+              sortStore.sort((a, b) => a[sortKey].localeCompare(b[sortKey]));
+              setnamedirection(null);
+              setaddressdirection('descending');
+              const slice = sortStore.slice(offset, offset + perPage);
+              setData(slice);
+              setorder('ASC');
+            }
+            if(order === 'ASC'){
+              sortStore.sort((a, b) => b[sortKey].localeCompare(a[sortKey]));
+              setnamedirection(null);
+              setaddressdirection('ascending');
+              const slice = sortStore.slice(offset, offset + perPage);
+              setData(slice);
+              setorder('DESC');
+            }
+            break;
+      default:
+          throw new Error()
+  }
 }
-  
+
+  const setPageSize = (e, data) => {
+    setPerPage(data.value);
+  }
   
   return(
     <div>
@@ -61,32 +128,50 @@ const Store = (props) => {
         <DeleteStoreModal store={store} opendelete={opendelete} handleModal={(value) => setopenDelete(value)} />
         <Button content="New Store" color="blue" onClick={() => setopen(true)}></Button>
         
-            <Table sortable striped>
+            <Table striped sortable celled fixed>
                 <Table.Header>
                     <Table.Row>
-                        <Table.HeaderCell>Name</Table.HeaderCell>
-                        <Table.HeaderCell>Address</Table.HeaderCell>
+                        <Table.HeaderCell sorted={namedirection} onClick={() => onSort('name', order)}>Name</Table.HeaderCell>
+                        <Table.HeaderCell sorted={addressdirection} onClick={() => onSort('address', order)}>Address</Table.HeaderCell>
                         <Table.HeaderCell>Actions</Table.HeaderCell>
                         <Table.HeaderCell>Actions</Table.HeaderCell>
                     </Table.Row>
                 </Table.Header>
-                <Table.Body>
-                    {stores.map((store, index) => {
-                        return(
-                            <Table.Row className="store-row" key ={store.storeId}>
-                                <Table.Cell>{store.name}</Table.Cell>
-                                <Table.Cell>{store.address}</Table.Cell>
-                                <Table.Cell>
-                                    <Button content="EDIT" color='yellow' icon='edit' labelPosition='left' onClick={() => handleEditStore(store)}></Button>
-                                </Table.Cell>
-                                <Table.Cell>
-                                    <Button content="DELETE" color='red' icon='edit' labelPosition='left' onClick={() => handleDeleteStore(store)}></Button>
-                                </Table.Cell>
-                            </Table.Row>
-                        )
-                    })}
+                <Table.Body className='tbody-class'>
+                {data.map(s => {
+                    return (
+                        <Table.Row className="store-row" key ={s.storeId}>
+                          <Table.Cell>{s.name}</Table.Cell>
+                          <Table.Cell>{s.address}</Table.Cell>
+                          <Table.Cell>
+                              <Button content="EDIT" color='yellow' icon='edit' labelPosition='left' onClick={() => handleEditStore(s)}></Button>
+                          </Table.Cell>
+                          <Table.Cell>
+                              <Button content="DELETE" color='red' icon='edit' labelPosition='left' onClick={() => handleDeleteStore(s)}></Button>
+                          </Table.Cell>
+                        </Table.Row>    
+                    )
+                })}
                 </Table.Body>
             </ Table>
+            <Select className="select-filter" defaultValue={perPage}
+                options={[5,10,20,30,40,50].map((p, index) => {
+                    return {
+                        key: index,
+                        value: p,
+                        text: p
+                    }
+                })} onChange={setPageSize}>
+            </Select>
+            <ReactPaginate
+                pageCount={pageCount}
+                marginPagesDisplayed={0}
+                pageRangeDisplayed={3}
+                onPageChange={handlePaginationClick}
+                containerClassName={"pagination"}
+                subContainerClassName={"pages pagination"}
+                activeClassName={"active"}
+            />
         </div>
     )
 }
