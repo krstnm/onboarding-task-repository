d0020b33534ffbee79ef34e46f0e15e0f7cbed6f
krstnm <75930370+krstnm@users.noreply.github.com>
2021-01-25 17:57:26 +1300

    Update Customer.jsx

--- src/components/Shop/CustomerFolder/Customer.jsx
+++ src/components/Shop/CustomerFolder/Customer.jsx
@@ -1,10 +1,11 @@
 import React, {useEffect, useState} from "react";
-import Axios from "axios";
-import { Button, Table } from "semantic-ui-react";
+import { Button, Table, Select } from "semantic-ui-react";
 import "semantic-ui-css/semantic.min.css";
 import CreateCustomerModal from "./CreateCustomerModal";
 import EditCustomerModal from "./EditCustomerModal";
 import DeleteCustomerModal from "./DeleteCustomerModal";
+import $ from 'jquery';
+import ReactPaginate from 'react-paginate';
 
 const Customer = (props) => {
   const [customers, setcustomers] = useState([]);
@@ -12,6 +13,13 @@ const Customer = (props) => {
   const [openEdit, setopenEdit] = useState(false);
   const [opendelete, setopenDelete] = useState(false);
   const [customer, setcustomer] = useState({});
+  const [order, setorder] = useState('DESC');
+  const [offset, setOffset] = useState(0);
+  const [data, setData] = useState([]);
+  const [perPage, setPerPage] = useState(5);
+  const [pageCount, setpageCount] = useState(0);
+  const [namedirection, setnamedirection] = useState();
+  const [addressdirection, setaddressdirection] = useState();
 
   useEffect(() => {
       if(!open){
@@ -23,23 +31,36 @@ const Customer = (props) => {
     if(!openEdit){
       getCustomers();
     }
-},[openEdit]);
+  },[openEdit]);
 
-useEffect(() => {
+  useEffect(() => {
     if(!opendelete){
       getCustomers();
     }
-},[opendelete]);
+  },[opendelete]);
 
+  useEffect(() => {
+    getCustomers();
+  }, [offset, perPage])
 
   const getCustomers = () => {
-    Axios.get("/Customers/GetCustomer")
-        .then((res) => {
-            setcustomers(res.data)
-        })
-        .catch((err) => {
-            console.log(err)
-    });
+    $.ajax({
+        method: "GET",
+        url: "/Customers/GetCustomer",
+        dataType: 'json',
+        success: function(res){
+            setcustomers(res)
+            const slice = res.slice(offset, offset + perPage);
+            setData(slice);
+            setpageCount(Math.ceil(res.length / perPage))
+        }
+    })
+  }
+
+  const handlePaginationClick = (e) => {
+    const selectedPage = e.selected;
+    const offsets = selectedPage * perPage;
+    setOffset(offsets);
   }
 
   const handleEditCustomer = (customer) => {
@@ -50,41 +71,110 @@ useEffect(() => {
   const handleDeleteCustomer = (customer) => {
     setcustomer(customer);
     setopenDelete(true);
-}
+  }
 
+  const onSort = (sortKey,order) => {
+      let sortCustomer = [...customers];
+      switch(sortKey){
+          case 'name':
+            if(order === 'DESC'){
+                sortCustomer.sort((a, b) => a[sortKey].localeCompare(b[sortKey]));
+                setnamedirection('descending');
+                setaddressdirection(null);
+                const slice = sortCustomer.slice(offset, offset + perPage);
+                setData(slice);
+                setorder('ASC');
+              }
+              if(order === 'ASC'){
+                sortCustomer.sort((a, b) => b[sortKey].localeCompare(a[sortKey]));
+                setnamedirection('ascending');
+                setaddressdirection(null);
+                const slice = sortCustomer.slice(offset, offset + perPage);
+                setData(slice);
+                setorder('DESC');
+              }
+            break;
+        case 'address':
+            if(order === 'DESC'){
+                sortCustomer.sort((a, b) => a[sortKey].localeCompare(b[sortKey]));
+                setnamedirection(null);
+                setaddressdirection('descending');
+                const slice = sortCustomer.slice(offset, offset + perPage);
+                setData(slice);
+                setorder('ASC');
+              }
+              if(order === 'ASC'){
+                sortCustomer.sort((a, b) => b[sortKey].localeCompare(a[sortKey]));
+                setnamedirection(null);
+                setaddressdirection('ascending');
+                const slice = sortCustomer.slice(offset, offset + perPage);
+                setData(slice);
+                setorder('DESC');
+              }
+              break;
+        default:
+            throw new Error()
+    }
+  }
+
+  const setPageSize = (e, data) => {
+    setPerPage(data.value);
+  }
+  
   return(
     <div>
-        <CreateCustomerModal open={open} handleModal={(value) => setopen(value)} />
-        <EditCustomerModal  customer={customer} openEdit={openEdit} handleModal={(value) => setopenEdit(value)} />
-        <DeleteCustomerModal customers={customers} customer={customer} opendelete={opendelete} handleModal={(value) => setopenDelete(value)} />
-        <Button content="New Customer" color="blue" onClick={() => setopen(true)}></Button>
-        
-            <Table sortable striped>
+        <div>
+            <CreateCustomerModal open={open} handleModal={(value) => setopen(value)} />
+            <EditCustomerModal  customer={customer} openEdit={openEdit} handleModal={(value) => setopenEdit(value)} />
+            <DeleteCustomerModal customers={customers} customer={customer} opendelete={opendelete} handleModal={(value) => setopenDelete(value)} />
+            <Button content="New Customer" color="blue" onClick={() => setopen(true)}></Button>
+        </div>
+        <div>
+            <Table sortable celled fixed>
                 <Table.Header>
                     <Table.Row>
-                        <Table.HeaderCell>Name</Table.HeaderCell>
-                        <Table.HeaderCell>Address</Table.HeaderCell>
+                        <Table.HeaderCell sorted={namedirection} onClick={() => onSort('name', order)}>Name</Table.HeaderCell>
+                        <Table.HeaderCell sorted={addressdirection} onClick={() => onSort('address', order)}>Address</Table.HeaderCell>
                         <Table.HeaderCell>Actions</Table.HeaderCell>
                         <Table.HeaderCell>Actions</Table.HeaderCell>
                     </Table.Row>
                 </Table.Header>
                 <Table.Body className='tbody-class'>
-                    {customers.map((customer, index) => {
-                        return(
-                            <Table.Row className="customer-row" key ={customer.customerId}>
-                                <Table.Cell>{customer.name}</Table.Cell>
-                                <Table.Cell>{customer.address}</Table.Cell>
-                                <Table.Cell>
-                                    <Button content="EDIT" color='yellow' icon='edit' labelPosition='left' onClick={() => handleEditCustomer(customer)}></Button>
-                                </Table.Cell>
-                                <Table.Cell>
-                                    <Button content="DELETE" color='red' icon='edit' labelPosition='left' onClick={() => handleDeleteCustomer(customer)}></Button>
-                                </Table.Cell>
-                            </Table.Row>
-                        )
-                    })}
-                </Table.Body>
+                {data.map(c => {
+                    return (
+                        <Table.Row className="customer-row" key ={c.customerId}>
+                            <Table.Cell>{c.name}</Table.Cell>
+                            <Table.Cell>{c.address}</Table.Cell>
+                            <Table.Cell>
+                                <Button content="EDIT" color='yellow' icon='edit' labelPosition='left' onClick={() => handleEditCustomer(c)}></Button>
+                            </Table.Cell>
+                            <Table.Cell>
+                                <Button content="DELETE" color='red' icon='edit' labelPosition='left' onClick={() => handleDeleteCustomer(c)}></Button>
+                            </Table.Cell>
+                        </Table.Row>  
+                    )
+                })}
+                </Table.Body>  
             </ Table>
+        <Select className="select-filter" defaultValue={perPage}
+        options={[5,10,20,30,40,50].map((p, index) => {
+            return {
+                key: index,
+                value: p,
+                text: p
+            }
+        })} onChange={setPageSize}>
+        </Select>
+        <ReactPaginate
+            pageCount={pageCount}
+            marginPagesDisplayed={0}
+            pageRangeDisplayed={3}
+            onPageChange={handlePaginationClick}
+            containerClassName={"pagination"}
+            subContainerClassName={"pages pagination"}
+            activeClassName={"active"}
+        />
+            </div>
         </div>
     )
 }
