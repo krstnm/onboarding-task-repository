07dc4bc1c8a41dee9ff40e0372aedb104d9e372b
krstnm <75930370+krstnm@users.noreply.github.com>
2021-01-25 18:01:22 +1300

    Update DeleteStoreModal.jsx

--- src/components/Shop/StoreFolder/DeleteStoreModal.jsx
+++ src/components/Shop/StoreFolder/DeleteStoreModal.jsx
@@ -1,35 +1,75 @@
-import React from "react";
-import Axios from "axios";
-import { Button, Modal} from "semantic-ui-react";
+import React, {useState, useEffect} from "react";
+import { Button, Form, Modal} from "semantic-ui-react";
 import $ from 'jquery';
 
-const DeleteStoreModal = (props) => {
-  const {opendelete, handleModal, store} = props;
+const EditStoreModal = (props) => {
+  const [name, setname] = useState();
+  const [address, setaddress] = useState();
+  const {openEdit, handleModal, store} = props;
+
+  useEffect(() => {
+    if(store){
+        setname(store.name);
+        setaddress(store.address);
+    }
+  }, [store])
   
-  const handleDeleteStore = (id) => {
+  const editStore = (id) => {
     $.ajax({
-      method: "DELETE",
-      url: `/Stores/DeleteStore/${id}`,
-      success: function(res){
-          handleModal(false);
-      }
+        method: "PUT",
+        url: `/Stores/PutStore/${id}`,
+        dataType: 'json',
+        data: JSON.stringify({ "storeId": id, "name": name, "address" : address }),
+        contentType: 'application/json',
+        success: function(res){
+            handleModal(false);
+        }
     })
   }
   
-  return (
-    <Modal open={opendelete} >
-        <Modal.Header>Delete customer</Modal.Header>
-        <Modal.Content>
-          <p>Are you sure?</p>
-        </Modal.Content>
-        <Modal.Actions>
-          <Button color='black' onClick={() => handleModal(false)}>
-            cancel
-          </Button>
-          <Button content="delete" color='red' icon='delete' labelPosition='left' onClick={() => handleDeleteStore(store.storeId)}/>
-        </Modal.Actions>
-    </Modal>
-  )
+  const handleChange = (e, field) => {
+    switch(field){
+        case "name":
+            setname(e.target.value);
+            break;
+        case "address":
+            setaddress(e.target.value);
+            break;
+        default:
+            break;
+    }
+  }
+
+  if(store){
+    return (
+        <Modal open={openEdit} >
+            <Modal.Header>Edit Store</Modal.Header>
+                <Modal.Content>
+                    <Form>
+                        <Form.Field>
+                            <label>NAME</label>
+                            <input defaultValue={name} 
+                                onChange={(event) => handleChange(event, "name")} />
+                        </Form.Field>
+                        <Form.Field>
+                            <label>ADDRESS</label>
+                            <input defaultValue={address} 
+                                onChange={(event) => handleChange(event, "address")} />
+                        </Form.Field>
+                    </Form>
+                </Modal.Content>
+            <Modal.Actions>
+                <Button color='black' onClick={() => handleModal(false)}>cancel</Button>
+                <Button content="edit" color='green' icon='checkmark' labelPosition='right' onClick={() => editStore(store.storeId)} positive></Button>
+            </Modal.Actions>
+        </Modal>
+    )
+  }
+  else {
+      return(
+          <div></div>
+      )
+  }
 }
   
-export default DeleteStoreModal
+export default EditStoreModal
