9604792c82a1feb204eaacea0028a2f969bb14c3
krstnm <75930370+krstnm@users.noreply.github.com>
2021-01-25 18:03:07 +1300

    Update CreateSalesModal.jsx

--- src/components/Shop/SalesFolder/CreateSalesModal.jsx
+++ src/components/Shop/SalesFolder/CreateSalesModal.jsx
@@ -2,30 +2,27 @@ import React, {useState} from "react";
 import Axios from "axios";
 import { Button, Form, Modal} from "semantic-ui-react";
 import DatePicker from "react-datepicker";
- 
 import "react-datepicker/dist/react-datepicker.css";
-
+import $ from 'jquery';
 
 const CreateSalesModal = (props) => {
   const [customer, setcustomer] = useState([]);
   const [product, setproduct] = useState([]);
   const [store, setstore] = useState([]);
   const [startDate, setStartDate] = useState(new Date());
-  const {open, handleModal, customers, products, stores} = props;
-  
+  const {handleModal, customers, products, stores} = props;
+  const [open, setOpen] = useState(false);
 
   const createSales = () => {
-    Axios.post('/Sales/PostSales', {
-        'customerId': customer,
-        'productId': product,
-        'storeId': store,
-        'dateSold': startDate
-    })
-    .then((res) => {
-        handleModal(false);
-    })
-    .catch((err) => {
-        console.log(err)
+    $.ajax({
+        method: "POST",
+        url: "/Sales/PostSales",
+        dataType: 'json',
+        data: JSON.stringify({ "customerId": customer, "productId" : product, "storeId" : store, "dateSold" : startDate }),
+        contentType: 'application/json',
+        success: function(res){
+            setOpen(false);
+        }
     })
   }
 
@@ -33,21 +30,24 @@ const CreateSalesModal = (props) => {
       handleModal(false);
   }
 
-  const handleDropdownChangeCustomer = (event, data) => {
-    setcustomer(data.value);
-  }
-  
-  const handleDropdownChangeProduct = (event, data) => {
-    setproduct(data.value);
-  }
-
-  const handleDropdownChangeStore = (event, data) => {
-    setstore(data.value);
-  }
-
+  const handleChange = (e, input) => {
+    switch (input) {
+      case "customer":
+        setcustomer(e.target.value);
+        break;
+      case "product":
+        setproduct(e.target.value);
+        break;
+      case "store":
+        setstore(e.target.value);
+        break;
+      default:
+        break;
+    }
+  };
 
   return (
-    <Modal open={open}>
+    <Modal onClose={() => setOpen(false)} onOpen={() => setOpen(true)} open={open} trigger={<Button primary>New Sale</Button>}>
         <Modal.Header>Create sales</Modal.Header>
             <Modal.Content>
                 <Form>
@@ -55,34 +55,51 @@ const CreateSalesModal = (props) => {
                         <label>Date Sold</label>
                     <DatePicker selected={startDate} label='Date Sold'  dateFormat="MM/dd/yyyy" onChange={date => setStartDate(date)} />
                     </Form.Field>
-                    <Form.Select fluid label='Customer' options={customers.map((c, index) => {
-                        return {
-                            key: index,
-                            text: c.label,
-                            value: c.value
-                        }
-                    })} onChange={handleDropdownChangeCustomer} >
-                    </Form.Select>
-                    <Form.Select fluid label='Product' options={products.map((p, index) => {
-                        return {
-                            key: index,
-                            text: p.label,
-                            value: p.value
-                        }
-                    })} onChange={handleDropdownChangeProduct} >
-                    </Form.Select>
-                    <Form.Select fluid label='Stores' options={stores.map((s, index) => {
-                        return {
-                            key: index,
-                            text: s.label,
-                            value: s.value
-                        }
-                    })} onChange={handleDropdownChangeStore} >
-                    </Form.Select>
+                    <Form.Field>
+                        <label>Customer</label>
+                        <select label='Customer' onChange={(e) => handleChange(e, "customer")} >
+                        <option value=""></option>
+                        {customers.map((customer, index) => {
+                            return (
+                            <option key={index} value={customer.customerId}>
+                                {customer.name}
+                            </option>
+                        );
+                        })}
+                        </select>
+                    </Form.Field>
+                    <Form.Field>
+                        <label>Product</label>
+                        <select label='Product' onChange={(e) => handleChange(e, "product")} >
+                        <option value=""></option>
+                        {products.map((product, index) => {
+                            return (
+                            <option key={index} value={product.productId}>
+                                {product.name}
+                            </option>
+                        );
+                        })}
+                        </select>
+                    </Form.Field>
+                    <Form.Field>
+                        <label>Store</label>
+                        <select label='Store' onChange={(e) => handleChange(e, "store")} >
+                        <option value=""></option>
+                        {stores.map((store, index) => {
+                            return (
+                            <option key={index} value={store.storeId}>
+                                {store.name}
+                            </option>
+                        );
+                        })}
+                        </select>
+                    </Form.Field>
                 </Form>
             </Modal.Content>
             <Modal.Actions>
-                <Button color='black' onClick={handleCancel}>cancel</Button>
+                <Button color="black" onClick={() => setOpen(false)}>
+                    cancel
+                </Button>
                 <Button content="create" color='green' icon='checkmark' labelPosition='right' onClick={createSales} positive></Button>
             </Modal.Actions>
         </Modal>
